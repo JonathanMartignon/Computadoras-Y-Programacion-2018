1a7bdc103e7d23049e773caa9ab24ca786913560
Martiñón Luna Jonathan José <31597046+JonathanMartignon@users.noreply.github.com>
2017-11-14 09:47:32 -0600

    Tarea 21
    Escribir nuestro nombre creando una biblioteca donde se encuentran las funciones de las letras a utilizar.

---
+++ Tarea_21/Ltrs.h
@@ -0,0 +1,42 @@
+char Pl(void){//Pl Primera Letra
+	printf("\tJ J J J\n\n");
+	printf("\t   J\n\n");
+	printf("\tJ  J\n\n");
+	printf("\tJ  J\n\n");
+	printf("\t J J\n\n");
+}
+char Sl(void){//Segunda Letra
+	printf("\t  O O  \n\n");
+	printf("\tO     O\n\n");
+	printf("\tO     O\n\n");
+	printf("\tO     O\n\n");
+	printf("\t  O O  \n\n");
+}
+char Tl(void){//Tercera Letra
+	printf("\tN     N\n\n");
+	printf("\tN N   N\n\n");
+	printf("\tN  N  N\n\n");
+	printf("\tN   N N\n\n");
+	printf("\tN     N\n\n");
+}
+char Cl(void){//Cuarta Letra
+    printf("\t  A A  \n\n");
+    printf("\tA     A\n\n");
+    printf("\tA A A A\n\n");
+    printf("\tA     A\n\n");
+    printf("\tA     A\n\n");
+}
+char Ql(void){//Quinta Letra
+	printf("\tT T T T\n\n");
+	printf("\t   T   \n\n");
+	printf("\t   T   \n\n");
+	printf("\t   T   \n\n");
+	printf("\t   T   \n\n");
+}
+char Sel(void){//Sexta letra
+	printf("\tH     H\n\n");
+	printf("\tH     H\n\n");
+	printf("\tH H H H\n\n");
+	printf("\tH     H\n\n");
+	printf("\tH     H\n\n");
+}//Las demas letras se repiten, no es necesario volver a hacer su código.
---
+++ Tarea_21/Tarea21.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Ltrs.h" //Se agrega la biblioteca que contiene las funciones de las letras
+
+int main(int argc, char *argv[]) {
+	
+	printf("\n\nMarti%c%cn Luna Jonathan Jos%c\nFES Arag%cn\nGrupo 1159\n\n",164,162,130,162);  /*Presentacion*/
+	printf("\tDescripci%cn de programa.\nPrograma que escribe nuestro nombre usando una biblioteca que contiene las letras de mismo.\n\n",162);
+    printf("%c%c%c%c%c%c%c%c",Tl(),Cl(),Sel(),Ql(),Cl(),Tl(),Sl(),Pl());
+    system("PAUSE");
+	return 0;
+}
